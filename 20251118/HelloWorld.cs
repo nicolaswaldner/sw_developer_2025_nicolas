bf71e3b63361c47a63311a6536ce7724e88b492d
Nicolas Waldner (WIFI) <nicolas.waldner1@gmail.com>
2025-11-20 18:31:20 +0100

    My first HelloWorld application

---
+++ 20251118/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DemoApplication
+{
+	public class Application
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+
+}
