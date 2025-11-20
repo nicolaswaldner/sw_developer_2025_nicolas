90c5a3251e32d5fd59fd83ac042a734e66d35e07
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-11-20 21:17:34 +0100

    My first VisualStudio project

---
+++ 20251120/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        //Haupteinstiegspunkt (Main)
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World...");
+            Console.ReadLine();
+            
+        }
+    }
+}
