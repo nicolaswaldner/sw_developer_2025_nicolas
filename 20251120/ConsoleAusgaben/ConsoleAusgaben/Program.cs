8ff87ba1ca777755d737b490241b9b44fa5e7a44
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-11-20 21:28:37 +0100

    Aufgabe Console Ausgaben

---
+++ 20251120/ConsoleAusgaben/ConsoleAusgaben/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAusgaben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Name: 
+             * Wohnort:
+             * 
+             *       Mein Motto:
+             */
+            Console.WriteLine("Name: Nicolas Waldner /n"
+        }
+    }
+}
