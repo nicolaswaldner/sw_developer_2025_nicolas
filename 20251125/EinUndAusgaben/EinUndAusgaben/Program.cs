6c1edcbb37f085de5b466c906d1857e534a721a7
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-11-25 21:37:08 +0100

    used a variable for name value

---
+++ 20251125/EinUndAusgaben/EinUndAusgaben/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinUndAusgaben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration
+            string nameOfUser;
+
+            //Initialisierung
+            nameOfUser = string.Empty;
+
+            //Deklaration & Initialisierung
+            string name = "Gandalf";
+
+            Console.WriteLine("Name:" + name);
+            Console.Write("Ort: Egg\n\n\t");
+            Console.WriteLine("Plz: 6963");
+            
+            
+
+
+
+        }
+    }
+}
