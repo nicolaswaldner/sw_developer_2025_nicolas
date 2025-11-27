58b504f5ed04a1e7ddbbaefbad82c6f83c6b5d41
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-11-27 20:56:01 +0100

    grundlagen bsp. mit eingaben in der console

---
+++ 20251127/EingabenGL/EingabenGL/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string eingabeAlter = string.Empty;
+            int geburtsJahr = 0;
+            int alter = 0;
+
+            Console.Write("Bitte Namen eingeben: ");
+            name = Console.ReadLine();
+
+            Console.Write("Bitte Alter eingeben: ");
+            eingabeAlter = Console.ReadLine();
+
+            //Konvertierung für alles was nicht string ist
+            alter = int.Parse(eingabeAlter);
+
+            //Berechnung
+            geburtsJahr = 2025 - alter;
+
+            Console.WriteLine("Ihr Name ist: " + name.ToUpper());
+            Console.WriteLine("Dein Geburtsjahr ist: " + geburtsJahr);
+
+
+
+        }
+    }
+}
