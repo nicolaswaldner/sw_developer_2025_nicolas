eb94da2c6bceeee7388805c519f6d89a8893b989
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-12-04 20:51:46 +0100

    iterationskonstrukte while

---
+++ 20251204/Iterationskonstrukte_while/Iterationskonstrukte_while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterationskonstrukte_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kopfgesteuerte Schleife
+            while (DateTime.Now.Hour == 18)
+            {
+                Console.WriteLine("Es ist noch immer 18:00 Uhr");
+            }
+
+            //fussgesteuerte Schleife; wird mind. einmal ausgeführt
+            do
+            {
+                Console.WriteLine("Es ist noch immer 18:00 Uhr");
+            }
+            while (DateTime.Now.Hour == 18);
+        }
+    }
+}
