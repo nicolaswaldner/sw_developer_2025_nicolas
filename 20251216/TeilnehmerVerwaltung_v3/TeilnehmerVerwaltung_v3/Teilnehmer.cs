243440c809a2159967d8620d99a29e46e95787cc
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-12-16 21:44:19 +0100

    create file

--- 20251216/TeilnehmerVerwaltung_v3/TeilnehmerVerwaltung_v3/Teilnehmer.cs
+++ 20251216/TeilnehmerVerwaltung_v3/TeilnehmerVerwaltung_v3/Teilnehmer.cs
@@ -8,6 +8,7 @@ namespace TeilnehmerVerwaltung_v3
 {
     public struct Teilnehmer
     {
+        public Guid Id;
         public string Name;
         public DateTime Geburtsdatum;
         public Adresse Wohnadresse;
