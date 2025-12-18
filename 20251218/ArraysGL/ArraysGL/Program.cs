553410b40f660836a4ddd469fc71147ac2b4c8aa
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-12-18 20:38:41 +0100

    Grundlagen Arrays

---
+++ 20251218/ArraysGL/ArraysGL/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArraysGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl;
+
+            //Deklaration
+            int[] zahlen;
+            string[] namen;
+            
+
+            //Dimensionierung
+            zahlen = new int[5]; //5 ist die Anzahl der Elemente in der Liste
+
+            zahlen[0] = 2;
+            zahlen[1] = -3;
+            zahlen[2] = 2543;
+            zahlen[3] = 1560;
+
+            Console.WriteLine("Wert 1 = " + zahlen[0]);
+        }
+    }
+}
