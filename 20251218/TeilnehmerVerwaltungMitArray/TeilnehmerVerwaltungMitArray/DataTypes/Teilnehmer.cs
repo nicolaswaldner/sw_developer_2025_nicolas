fb7a82980c703dc97b14d306383b7fea3e9c0150
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2025-12-18 20:39:22 +0100

    TeilnehmerVerwaltung mit Arrays (nur Eingabe)

---
+++ 20251218/TeilnehmerVerwaltungMitArray/TeilnehmerVerwaltungMitArray/DataTypes/Teilnehmer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeilnehmerVerwaltungMitArray.DataTypes
+{
+    public struct Teilnehmer
+    {
+        public Guid Id;
+        public string Name;
+        public DateTime Geburtsdatum;
+        public Adresse Wohnadresse;
+    }
+}
