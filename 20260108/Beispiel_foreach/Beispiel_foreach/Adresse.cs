4bef2f8e30ca5a19438ced1903cd0df16bcef818
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-08 20:05:23 +0100

    foreach interation grundlagen

---
+++ 20260108/Beispiel_foreach/Beispiel_foreach/Adresse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beispiel_foreach
+{
+    public struct Adresse
+    {
+        public string Wohnort;
+        public int Plz;
+        public string Strasse;
+        public string HausNr;
+    }
+}
