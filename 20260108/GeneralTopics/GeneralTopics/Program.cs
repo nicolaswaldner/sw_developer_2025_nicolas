1f23a9c3de6373d627f7d584e4ea68de2cd81f25
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-08 20:05:46 +0100

    general topics für c#

---
+++ 20260108/GeneralTopics/GeneralTopics/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GeneralTopics
+{
+    internal class Program
+    {
+        //Konstanten
+        const ConsoleColor DefaultForegroundColor = ConsoleColor.Cyan;
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = Program.DefaultForegroundColor;
+
+            Console.WriteLine("Hallo zusammen!");
+            Console.ResetColor();
+
+            //hart-kodierte Werte (hard-coded)
+            
+
+            //Program.DefaultForegroundColor = ConsoleColor.Yellow;
+
+//####################################################################################################################
+
+            bool isValid = false;
+
+            if (isValid)
+            {
+                Console.WriteLine("is true");
+            }
+
+        }
+    }
+}
