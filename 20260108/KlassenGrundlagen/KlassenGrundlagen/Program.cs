f27565f1bebd6f480c255a299ae1d1beb6e18c64
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-08 21:35:21 +0100

    klassen grundlagen

---
+++ 20260108/KlassenGrundlagen/KlassenGrundlagen/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace KlassenGrundlagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee ist die Klasse
+            //ma ist das Objekt/Instanz
+
+            //Instanziierung
+            Employee ma = new Employee("Max", "Mustermann", new DateTime(2006, 2, 15));
+
+            ////Initialisierung
+            //ma.Name = "Max";
+            //ma.Surname = "Mustermann";
+            //ma.Id = Guid.NewGuid();
+            //ma.Salary = 1700.0m;
+            //ma.Birthday = new DateTime(2006, 2, 15);
+
+            ma.GiveBonus(0.1);
+            ma.Display();
+
+            //bonus geben
+            ma.GiveBonus(0.1);
+
+            //neues Gehalt darstellen
+            Console.WriteLine($"Neues Gehalt: EUR {ma.Salary.ToString("#,###.00")}");
+        }
+    }
+}
