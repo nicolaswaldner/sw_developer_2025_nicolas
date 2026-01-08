79828075543394e610d94ab852afc62de2822e17
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-08 20:06:24 +0100

    adresse change namespace

--- 20251218/TeilnehmerVerwaltungMitArray/TeilnehmerVerwaltungMitArray/DataTypes/Adresse.cs
+++ 20251218/TeilnehmerVerwaltungMitArray/TeilnehmerVerwaltungMitArray/DataTypes/Adresse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TeilnehmerVerwaltungMitArray.DataTypes
+namespace Beispiel_foreach
 {
     public struct Adresse
     {
