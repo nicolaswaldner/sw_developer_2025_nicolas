45b40e69cb9900547f1dcfd131dde3583a302fbf
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-15 19:31:28 +0100

    grundlagen klassen kapselung wiederholung

---
+++ 20260115/Vererbung_GL/Vererbung_GL/Car.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vererbung_GL
+{
+    public class Car
+    {
+        //Zustandsinformationen
+        private int _maxSpeed;
+        private int _currentSpeed;
+        private string _description;
+        private ConsoleColor _color;
+
+        //Standard Konstruktor
+        public Car()
+        {
+            _maxSpeed = 180;
+            _currentSpeed = 0;
+            _description = "default description";
+            _color = ConsoleColor.White;
+        }
+
+        //Konstruktor
+        public Car(string description)
+        {
+            _maxSpeed = 180;
+            _currentSpeed = 0;
+            _description = description;
+            _color = ConsoleColor.White;
+        }
+
+        public Car (string description, int maxSpeed, ConsoleColor color)
+        {
+            _description = description;
+            _maxSpeed = maxSpeed;
+            _currentSpeed = 0;
+            _color = color;
+        }
+
+        //Methode
+        public void SpeedUp(int delta)
+        {
+            _currentSpeed += delta;
+
+            if (_currentSpeed < 0)
+            {
+                _currentSpeed = 0;
+            }
+
+            if (_currentSpeed > _maxSpeed)
+            {
+                _currentSpeed = _maxSpeed;
+            }
+        }
+
+        public void Show()
+        {
+            ConsoleColor oldcolor = Console.ForegroundColor;
+            Console.ForegroundColor = _color;
+
+            Console.WriteLine($"{_description} [{_currentSpeed}/{_maxSpeed} km/h]");
+
+            Console.ForegroundColor = oldcolor;
+        }
+
+
+        //Eigenschaften/Properties
+        public int MaxSpeed 
+        { 
+            get => _maxSpeed; 
+        }
+
+        public int CurrentSpeed 
+        { 
+            get => _currentSpeed;  
+        }
+
+        public string Description 
+        { 
+            get => _description;  
+        }
+
+        public ConsoleColor Color 
+        { 
+            get => _color; 
+        }
+    }
+}
---
+++ 20260115/Vererbung_GL/Vererbung_GL/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Vererbung_GL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("VW Polo");
+            Car car2 = new Car("BadMobil V12", 350, ConsoleColor.DarkBlue);
+
+            car1.Show();
+            car2.Show();
+
+            car1.SpeedUp(100);
+            car1.Show();
+
+        }
+    }
+}
