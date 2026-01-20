db712a9c621bed09a29decaefe5c0947184ac706
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-01-20 19:36:18 +0100

    klassische vererbung, ist ein beziehung

---
+++ 20260120/Vererbung_Beispiel/Vererbung_Beispiel/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Vererbung_Beispiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var zahl = 5;
+
+            var randomNumberGenerator = CreateRandomGenerator();
+
+            var randomString = randomNumberGenerator.NextString(15);
+            Console.WriteLine(randomString);
+        }
+
+        private static RandomAdvanced CreateRandomGenerator()
+        {
+            return new RandomAdvanced();
+        }
+    }
+}
