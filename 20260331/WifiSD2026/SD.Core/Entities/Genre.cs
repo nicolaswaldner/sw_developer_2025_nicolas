9f31a4cf0aa2c8866ffa9cb3d6f80477c18fc081
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-03-31 21:31:37 +0200

    added folders

---
+++ 20260331/WifiSD2026/SD.Core/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SD.Core.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
