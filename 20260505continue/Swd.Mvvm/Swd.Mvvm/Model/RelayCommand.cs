c5a975c5e86f2f14f70bfc58c17cbf36f5f35b3e
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-05-07 20:05:31 +0200

    added nugetpacket mvvm community

---
+++ 20260505continue/Swd.Mvvm/Swd.Mvvm/Model/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Swd.Mvvm.Model
+{
+    public class RelayCommand : ICommand
+    {
+
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+
+        //Konstruktor
+        public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged;
+        
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
