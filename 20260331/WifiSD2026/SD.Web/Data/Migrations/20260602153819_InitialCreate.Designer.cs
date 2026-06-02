c69e7badf91d45a7e527c285a3f6d8ab56576ea0
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-06-02 18:00:41 +0200

    added migration (home)

--- 20260331/WifiSD2026/SD.Web/Data/Migrations/20260602153819_InitialCreate.Designer.cs
+++ 20260331/WifiSD2026/SD.Web/Data/Migrations/20260602153819_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SD.Web.Data;
 
@@ -11,9 +12,11 @@ using SD.Web.Data;
 namespace SD.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260602153819_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
