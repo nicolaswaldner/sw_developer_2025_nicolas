8116016e9a2b48c73a11beb8c65c164ff079bff4
nicolas waldner (wifi) <nicolas.waldner1@gmail.com>
2026-06-02 21:22:11 +0200

    added mediatorbasecontroller

---
+++ 20260331/WifiSD2026/SD.Web/Controllers/MediatorBaseController.cs
@@ -0,0 +1,16 @@
+﻿using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SD.Web.Controllers
+{
+    public class MediatorBaseController : Controller
+    {
+        private IMediator mediator;
+
+        //Konstante für ID Parameter
+        protected const string ID_PARAMETER = "/{Id}";
+
+        protected IMediator Mediator => mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    }
+}
